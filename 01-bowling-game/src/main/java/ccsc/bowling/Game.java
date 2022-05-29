3afbf775afd72ef16501256f5dcf43f9bd92ad9b
Falk Sippach <falk@jug-da.de>
2022-05-29 21:28:50 +0200

    Bowling Game: Schritt 7 (Game Klasse umbauen)

--- 01-bowling-game/src/main/java/ccsc/bowling/Game.java
+++ 01-bowling-game/src/main/java/ccsc/bowling/Game.java
@@ -2,13 +2,18 @@ package ccsc.bowling;
 
 public class Game {
 
-    private int score = 0;
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
-        score += pins;
+        rolls[currentRoll++] = pins;
     }
 
     public int getScore() {
+        int score = 0;
+        for (int i = 0; i < rolls.length; i++) {
+            score += rolls[i];
+        }
         return score;
     }
 }
