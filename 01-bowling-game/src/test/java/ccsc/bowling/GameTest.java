4e76cabc2f9531d2d5108cf401cd2c4fc466ced8
Falk Sippach <falk@jug-da.de>
2022-05-29 20:31:26 +0200

    Bowling Game: Schritt 1 (Testklasse angelegt)

---
+++ 01-bowling-game/src/test/java/ccsc/bowling/GameTest.java
@@ -0,0 +1,22 @@
+package ccsc.bowling;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * TODOs
+ *
+ * 1. Test: can roll gutter (no pins at all)
+ * 2. Test: can roll One's 20 times
+ * 3. Test: Spare
+ * 4. Test: Strike
+ * 5. Test: Perfect Match
+ */
+class GameTest {
+
+    @Test
+    void can_create_game() {
+        assertTrue(true);
+    }
+}
