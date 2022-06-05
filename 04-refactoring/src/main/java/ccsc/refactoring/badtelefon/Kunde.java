c716a44734257bd45aa7ef371735f5bc2729b2bd
Falk Sippach <falk@jug-da.de>
2022-06-05 22:35:14 +0200

    Refactoring Runde 2 (ermittlePreis extrahieren und nach Tarif verschieben, Kunde aufräumen)

--- 04-refactoring/src/main/java/ccsc/refactoring/badtelefon/Kunde.java
+++ 04-refactoring/src/main/java/ccsc/refactoring/badtelefon/Kunde.java
@@ -1,41 +1,15 @@
 package ccsc.refactoring.badtelefon;
 
 public class Kunde {
-	double gebuehr = 0.0;
-	Tarif tarif;
+	private double gebuehr = 0.0;
+	private final Tarif tarif;
 
 	public Kunde(int tarifArt) {
 		this.tarif = new Tarif(tarifArt);
 	}
 
-	public void account(int minuten, Zeitpunkt zeitpunkt) {
-		double preis = 0;
-
-		// Gespraechspreis ermitteln
-		switch (tarif.tarif) {
-		case Tarif.PRIVAT:
-			minuten = minuten - 1;
-			minuten = minuten < 0 ? 0 : minuten;
-			if (zeitpunkt.isMondschein())
-				preis = minuten * 0.69;
-			else
-				preis = minuten * 1.99;
-			break;
-
-		case Tarif.BUSINESS:
-			if (zeitpunkt.isMondschein())
-				preis = minuten * 0.79;
-			else
-				preis = minuten * 1.29;
-			break;
-
-		case Tarif.PROFI:
-			preis = minuten * 0.69;
-			break;
-
-		}
-
-		gebuehr += preis;
+	public void bucheGespraech(int dauer, Zeitpunkt zeitpunkt) {
+		gebuehr += tarif.ermittlePreis(dauer, zeitpunkt);
 	}
 
 	public double getGebuehr() {
--- 04-refactoring/src/main/java/ccsc/refactoring/badtelefon/Tarif.java
+++ 04-refactoring/src/main/java/ccsc/refactoring/badtelefon/Tarif.java
@@ -10,4 +10,33 @@ public class Tarif {
 	public Tarif(int tarif) {
 		this.tarif = tarif;
 	}
+
+    public double ermittlePreis(int dauer, Zeitpunkt zeitpunkt) {
+        double preis = 0;
+
+        // Gespraechspreis ermitteln
+        switch (this.tarif) {
+        case PRIVAT:
+            dauer = dauer - 1;
+            dauer = dauer < 0 ? 0 : dauer;
+            if (zeitpunkt.isMondschein())
+                preis = dauer * 0.69;
+            else
+                preis = dauer * 1.99;
+            break;
+
+        case BUSINESS:
+            if (zeitpunkt.isMondschein())
+                preis = dauer * 0.79;
+            else
+                preis = dauer * 1.29;
+            break;
+
+        case PROFI:
+            preis = dauer * 0.69;
+            break;
+
+        }
+        return preis;
+    }
 }
