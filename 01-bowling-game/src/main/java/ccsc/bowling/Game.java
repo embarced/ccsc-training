606f701bd705fbef7895d42f975bedffdb893640
Falk Sippach <falk@jug-da.de>
2023-03-16 09:27:50 +0100

    Übung Bowling Kata Teil 2

--- 01-bowling-game/src/main/java/ccsc/bowling/Game.java
+++ 01-bowling-game/src/main/java/ccsc/bowling/Game.java
@@ -1,14 +1,35 @@
 package ccsc.bowling;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Game {
     private int pins = 0;
-    private int lastPins = 0;
+    private int rolls[] = new int[21];
+    private short currentRoll = 0;
 
     public void roll(int fallenPins) {
         this.pins += fallenPins;
+        rolls[currentRoll] = fallenPins;
+        currentRoll++;
     }
 
     public int score() {
-        return this.pins;
+        int score = 0;
+        int i = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            boolean isSpare = rolls[i] + rolls[i + 1] == 10;
+            if (isSpare) {
+                score += 10 + rolls[i + 2];
+            } else {
+                score += rolls[i] + rolls[i + 1];
+            }
+            i += 2;
+        }
+        return score;
+    }
+
+    public int score2() {
+        return IntStream.of(rolls).reduce(0, (all, el) -> all + el);
     }
 }
--- 01-bowling-game/src/test/java/ccsc/bowling/GameTest.java
+++ 01-bowling-game/src/test/java/ccsc/bowling/GameTest.java
@@ -1,9 +1,18 @@
 package ccsc.bowling;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -15,9 +24,23 @@ import static org.junit.jupiter.api.Assertions.*;
  * 4. Test: Strike
  * 5. Test: Perfect Match
  */
+@ExtendWith(MockitoExtension.class)
 public class GameTest {
 
     private Game game;
+
+    @Mock
+    private Game mockGame;
+
+    @Test
+    void mock_is_able_to_return_perfect_game_score() {
+        Mockito.when(mockGame.score()).thenReturn(300);
+
+        assertThat(mockGame.score()).isEqualTo(300);
+        assertThat(mockGame.score()).isEqualTo(300);
+
+        Mockito.verify(mockGame, Mockito.times(1)).score();
+    }
 
     @BeforeEach
     void createGame() {
@@ -39,16 +62,36 @@ public class GameTest {
     @Test
     void can_roll_all_ones() {
         rollManyTimes(20, 1);
-        assertEquals(20, game.score());
+        assertEquals(20, game.score2());
+        org.assertj.core.api.Assertions.assertThat(game.score())
+                .as("Score ist ..")
+                .isEqualTo(20);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4})
+    void can_roll_all_the_same_numbers(Integer pins) {
+        rollManyTimes(20, pins);
+        assertThat(game.score()).isGreaterThan(0);
+    }
+
+    @ParameterizedTest(name = "{index}: 20 rolls of {0} pin(s) leads to a score of {1}")
+    @CsvFileSource(resources = "/pins.csv", numLinesToSkip = 1)
+    void can_roll_all_the_same_numbers_again(Integer pins, Integer score) {
+        rollManyTimes(20, pins);
+        assertThat(game.score()).isEqualTo(score);
     }
 
     @Test
-    @Disabled
     void can_roll_a_spare() {
-        game.roll(3);
-        game.roll(7);
+        rollSpare();
         game.roll(5);
         rollManyTimes(17, 0);
         assertEquals(20, game.score());
+    }
+
+    private void rollSpare() {
+        game.roll(3);
+        game.roll(7);
     }
 }
